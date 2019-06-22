d9c49ee8054dbdf16eab8994d98d3ca461e53cea
barry <jobael.bari@g.msuiit.edu.ph>
2019-06-22 19:06:42 +0800

    Issue Contact Not Show

--- resources/views/pages/messenger.blade.php
+++ resources/views/pages/messenger.blade.php
@@ -65,11 +65,11 @@
                 </div>
             </div>
 
-            <div class="box box-solid">
+            <div class="box box-solid collapsed-box">
                 <div class="box-header with-border">
                 <h3 class="box-title">Group</h3>
                 <div class="box-tools">
-                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
                         </button>
                     </div>
                     </div>
@@ -80,14 +80,37 @@
                     </div>
                 <!-- /.box-body -->
             </div>
+
+            <div class="box box-solid collapsed-box">
+                <div class="box-header with-border">
+                <h3 class="box-title">All Contacts</h3>
+                <div class="box-tools">
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
+                        </button>
+                    </div>
+                    </div>
+                    <div class="box-body no-padding">
+                    <ul class="nav nav-pills nav-stacked">
+                        @if(count($users) > 0)
+                            @foreach($users as $user)
+                            @csrf
+                                {{-- @if($user->fullName() != 'Admin Admin') --}}
+                                    <li value="{{$user->id}}" ><a href="#"><span>{{$user->first_name}}</span> </a></li>
+                                {{-- @endif --}}
+                            @endforeach
+                         @endif
+                    </ul>
+                    </div>
+                <!-- /.box-body -->
+            </div>
             </div>
             <!-- /.col -->
 
             <div class="col-md-6">
             <!-- DIRECT CHAT PRIMARY -->
             <div class="box box-primary direct-chat direct-chat-primary">
-                <div class="box-header with-border">
-                <h3 class="box-title">Direct Chat</h3>
+                <div id='h3name' class="box-header with-border">
+                <h3 id='h3name' class="box-title">Direct Chat</h3>
     
                 <div class="box-tools pull-right">
                     <span data-toggle="tooltip" title="3 New Messages" class="badge bg-light-blue">3</span>
@@ -105,11 +128,11 @@
                 <!-- /.box-body -->
                 <div class="box-footer">
                     <div id="typingStatus" class="col-lg-12" style="padding: 15px"></div>
-                    <div class="input-group">
-                    <input id="text" type="text" name="message" placeholder="Type Message ..." class="form-control">
-                        <span class="input-group-btn">
-                            <button type="submit" class="btn btn-primary btn-flat">Send</button>
-                        </span>
+                    <div class="input-group">                    
+                        <input id="text" type="text" name="message" placeholder="Type Message ..." class="form-control">
+                            <span class="input-group-btn">
+                                <button type="reset" onClick="sendMessage();" class="btn btn-primary btn-flat">Send</button>
+                            </span>
                     </div>
                 
                 </div>
@@ -145,29 +168,29 @@
     </div>
     <script src="/bower_components/jquery/dist/jquery.min.js"></script>
 <script>
-var i,user,chat;
+var user,chat;
 
 $(document).ready(function()
-{
+{   
+    $('#text').val('');
     $("li").click(function(){
     if (!$(this).hasClass("active")) {
     // Remove the class from anything that is active
     $("li.active").removeClass("active");
     $('div.direct-chat-msg').remove();
+    $('#text').val('');
     // $("li.active").removeAttr("disabled");
-    $(this).addClass("active").attr("disabled");
+    $(this).addClass("active").attr("disabled");    
     // $(this).attr("disabled");
-    
     user = {{Auth::user()->id}};
     chat = $(this).attr('value');
-    $(document).keyup(function(e) {
+    $("#text").keyup(function(e) {
         if (e.keyCode == 13){            
-            sendMessage();  
+             
         }
         else{isTyping();}
     });
-    i = 0;
-    retrieveExistingMessages(i);
+    retrieveExistingMessages();
     pullData();
     }; 
     });
@@ -181,9 +204,8 @@ function pullData()
     setTimeout(pullData,1500);
 }
 
-function retrieveExistingMessages(i)
+function retrieveExistingMessages()
 {  
-    if(i == 0){ 
     $.post('/retrieveExistingMessages', {_token:"{{csrf_token()}}",user: user, chat: chat}, function(data)
     {
         for (i = 0; i < data.length; i++) { 
@@ -196,7 +218,6 @@ function retrieveExistingMessages(i)
             '<img class="direct-chat-img" src="../dist/img/user1-128x128.jpg" alt="Message User Image">'+
             '<div class="direct-chat-text">'+data[i]['message']+'</div>'+
             '</div>';
-            console.log(i);
             $('div.direct-chat-messages').append(message);
         }else{
             var message = '<div class="direct-chat-msg">' +
@@ -208,14 +229,9 @@ function retrieveExistingMessages(i)
             '<div class="direct-chat-text">'+data[i]['message']+'</div>'+
             '</div>';
             $('div.direct-chat-messages').append(message);
-            console.log(i+'fale');
         }
         }
     });
-    console.log(i);
-    i = 1;
-    console.log(i);
-    }
 }
 
 function retrieveChatMessages()
@@ -265,6 +281,7 @@ function sendMessage()
                 '<div class="direct-chat-text">'+data['message']+'</div>'+
                 '</div>';
                 $('div.direct-chat-messages').append(message);
+                $('#text').val('');
                 notTyping();
             // }
         });
